7254046d398742e55d644c9d9aed705593d44637
IsraelCavalcanti <israel_cavalcanti@hotmail.com>
2020-06-10 16:38:06 -0300

    Começando o header, footer e o card

--- src/components/Comprar.js
+++ src/components/Comprar.js
@@ -1,14 +1,18 @@
-import React from 'react'
-
-import CardVenda from './CardVenda'
+import React from "react";
+import CardVenda from "./CardVenda";
+import Header from "./Header";
+import Footer from "./Footer";
 
 class Comprar extends React.Component {
-    render () {
-        return (
-            <CardVenda />
-        )
-    }    
+  render() {
+    return (
+      <div>
+        <Header />
+        <CardVenda />
+        <Footer />
+      </div>
+    );
+  }
 }
-
 
 export default Comprar;
